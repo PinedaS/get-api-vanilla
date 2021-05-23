560e3ff762b3d91df8071a20fd2129a7da2b5aa6
Sebastián Pineda <spineda0424@gmail.com>
2021-05-23 14:42:13 -0500

    Corregido bug de los botones de Anterior y Siguiente

--- index.js
+++ index.js
@@ -7,38 +7,56 @@ const getData = async (API_URL) => {
 
 const printData = async (API_URL) => {
   const data = await getData(API_URL);
-  const characters = await data.results;
+  const results = await data.results;
 
-  characters.forEach((c) => {
-    // Nodos creados en el DOM
+  results.forEach((r) => {
+    // Nodos de la grilla de Bootstrap
     const divElement = document.createElement("div");
     divElement.className = "col";
     const rowElement = document.querySelector("div.row");
 
-    // Atributos de la API
-    const name = c.name;
-    const specie = c.species;
-    const gender = c.gender;
-    const location = c.location.name;
-    const image = c.image;
+    const name = r.name;
+    const specie = r.species;
+    const gender = r.gender;
+    const location = r.location.name;
+    const image = r.image;
 
     divElement.innerHTML = `<div class="card" style="width: 18rem;">
-    <img src="${c.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${name}</h5>
-      <p class="card-text">Especie: ${specie}</p>
-      <p class="card-text">Género: ${gender}</p>
-      <p class="card-text">Ubicación: ${location}</p>
-    </div>
-  </div>`;
+      <img src="${image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">Especie: ${specie}</p>
+        <p class="card-text">Género: ${gender}</p>
+        <p class="card-text">Ubicación: ${location}</p>
+      </div>
+    </div>`;
+
     rowElement.append(divElement);
   });
 };
 
 const pagination = async () => {
   let page = 1;
+  const startButton = document.querySelector(".navbar-brand");
   const next = document.querySelector("#next");
   const prev = document.querySelector("#prev");
+
+  if (page === 1) {
+    prev.disabled = true;
+  } else {
+    prev.disabled = false;
+  }
+
+  startButton.addEventListener("click", () => {
+    page = 1;
+
+    const nodeList = document.querySelectorAll("div.col");
+    nodeList.forEach((e) => {
+      e.remove();
+    });
+
+    printData("https://rickandmortyapi.com/api/character");
+  });
 
   next.addEventListener("click", async () => {
     // Se elimina el contenido de las columnas de la grilla de Bootstrap, para refrescar al cambiar de página
@@ -56,10 +74,10 @@ const pagination = async () => {
 
     page += 1;
 
-    // Habilitar el botón de "Anterior" cuando se ubica en la página 2 o superior
-    if (page > 1) {
-      const prevElement = document.querySelector("#prev");
-      prevElement.className = "page-item";
+    if (page === 1) {
+      prev.disabled = true;
+    } else {
+      prev.disabled = false;
     }
   });
 
@@ -78,17 +96,16 @@ const pagination = async () => {
 
     page -= 1;
 
-    // Habilitar el botón de "Anterior" cuando se ubica en la página 2 o superior
-    if (page > 1) {
-      const prevElement = document.querySelector("#prev");
-      prevElement.className = "page-item";
+    if (page === 1) {
+      prev.disabled = true;
+    } else {
+      prev.disabled = false;
     }
   });
 };
 
 const main = () => {
-  const API_URL = "https://rickandmortyapi.com/api/character";
-  printData(API_URL);
+  printData("https://rickandmortyapi.com/api/character");
 
   pagination();
 };
